cb2defb30714411c444e081998cc6a855e852713
khoihthe181332 <hatuankhoi2004@gmail.com>
2025-08-13 15:41:03 +0700

    nop bai day 22

---
+++ main.js
@@ -0,0 +1,65 @@
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#todo-input');
+const tasksList = document.querySelector('#task-list');
+// console.log(taskForm, taskInput, taskName);
+
+// lấy dữ diệu 
+const tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
+
+taskForm.onsubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+        name: taskInput.value,
+        isCompleted: false,
+    };
+
+    // kiểm tra trống
+    if (!newTask.name) {
+        alert("Chưa nhập task");
+        return;
+    }
+
+    // Kiểm tra lần nhập sau có bị trùng với lần nhập trước k
+    const valid = tasks.find(task => {
+        return task.name === taskInput.value;
+    });
+
+    if (valid) {
+        alert("Task đã tồn tại");
+        taskInput.value = "";
+        return;
+    }
+
+    // thêm task vào đầu danh sách
+    tasks.unshift(newTask);
+
+    // lưu vào storeage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // render task
+    renderTasks();
+
+    taskInput.value = "";
+}
+
+function renderTasks() {
+    const html = tasks.map(task => {
+        if (task.name) {
+            return `
+                <li class="task-item${task.isCompleted ? ' completed' : ''}">
+                    <span class="task-title">${task.name}</span>
+                    <div class="task-action">
+                        <button class="task-btn edit">Edit</button>
+                        <button class="task-btn done">Mark as done</button>
+                        <button class="task-btn delete">Delete</button>
+                    </div>
+                </li>
+            `;
+        }
+    }).join("");
+
+    tasksList.innerHTML = html;
+    // console.log(html);
+}
+
+renderTasks();
